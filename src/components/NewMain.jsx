2e2e5bb7ee91a9ddda2f5e01bfdf2f7be6f080f4
matousss <matousss33@gmail.com>
2022-04-20 21:52:16 +0200

    method hook

--- src/components/NewMain.jsx
+++ src/components/NewMain.jsx
@@ -7,7 +7,7 @@ export default function NewMain(props) {
 
     return (
         <div className='h-screen'>
-            <Sidebar user={props.user} setUser={props.setUser} setActiveConversation={setActiveConversation} setPage={props.setPage}/>
+            <Sidebar user={props.user} setUser={props.setUser} clearDesk={props.clearDesk} setActiveConversation={setActiveConversation}/>
             <MessagePanel user={props.user} activeConversation={activeConversation} setActiveConversation={setActiveConversation}/>
         </div>
     );
