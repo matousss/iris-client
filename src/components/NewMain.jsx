208686188f8ef02771843287a09840c7c9d8069b
matousss <matousss33@gmail.com>
2022-04-23 17:24:51 +0200

    Channel visualization

--- src/components/NewMain.jsx
+++ src/components/NewMain.jsx
@@ -1,65 +1,22 @@
 import React, {useEffect, useState} from 'react';
 import Sidebar from "./Sidebar";
 import MessagePanel from "./MessagePanel";
+import {Channel, User} from "../utils/ModelStorage";
+
+
 
 export default function NewMain(props) {
     const [activeConversation, setActiveConversation] = useState('');
 
     return (
+
         <div className='h-screen'>
-            <Sidebar user={props.user} setUser={props.setUser} clearDesk={props.clearDesk} setActiveConversation={setActiveConversation} channels={props.channels}/>
-            <MessagePanel user={props.user} activeConversation={activeConversation} setActiveConversation={setActiveConversation}/>
+            <Sidebar user={props.user} setUser={props.setUser} clearDesk={props.clearDesk}
+                     setActiveConversation={setActiveConversation} channels={props.channels} storage={props.storage}/>
+            <MessagePanel user={props.user} activeConversation={activeConversation}
+                          setActiveConversation={setActiveConversation}/>
         </div>
     );
 }
 
 
-//
-// class NewMain extends React.Component {
-//     #ws;
-//     state = {activeConversation: ""}
-//
-//     constructor(props, context) {
-//         super(props, context);
-//         this.connect()
-//     }
-//
-//
-//     connect() {
-//         // set loading screen
-//         let ws = new WebSocket(WS_URL + '?token=' + loadToken())
-//         ws.onopen = (event) => {
-//             console.log(event);
-//             // remove loading screen?
-//         };
-//         ws.onclose = () => {
-//             this.connect();
-//         };
-//
-//         this.#ws = ws;
-//     }
-//
-//
-//     setActiveConversation = (conversation) => {
-//         if (this.state.activeConversation === conversation) return
-//         console.log(conversation)
-//         this.setState({activeConversation: conversation})
-//     }
-//
-//
-//     render() {
-//         let props = this.props;
-//         return (
-//             <div className='h-screen'>
-//                 <Sidebar user={props.user} setUser={props.setUser} setActiveConversation={this.setActiveConversation}
-//                          setPage={props.setPage}/>
-//                 <MessagePanel user={props.user} activeConversation={() => this.activeConversation}
-//                               setActiveConversation={this.setActiveConversation}/>
-//             </div>
-//         );
-//
-//     }
-// }
-//
-//
-// export default NewMain
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,81 +9,38 @@ import {getMiniProfile} from "../utils/RequestUtils";
 //temp import
 import avatar from '../assets/avatar.svg'
 
-async function getProfile(user) {
-    const response = await getMiniProfile(user).catch(function (e) {
-        console.log(e)
-    });
-
-    if (response.ok) {
-        return  await response.json();
-    } else {
-        return {
-            "user": user, "username": "NaN", "avatar": null
-        };
-    }
-}
-
-async function loadUsers(channelObject) {
-    let users = channelObject['users'];
-    let loaded = sessionStorage.getItem('users');
-
-    let cachedUsers = loaded === null ? {} : JSON.parse(loaded);
-    if (users !== null) {
-        for(const user of users) {
-            {
-                if (!(user in Object.keys(cachedUsers))) {
-                    let userObj = await getProfile(user);
-                    console.log(userObj)
-                }
-            }
-        }
-    }
-
-    sessionStorage.setItem('users', JSON.stringify(cachedUsers));
-    console.log(`cachedUsers: ${cachedUsers}`)
-    return cachedUsers;
-}
-
 
 export default function Sidebar(props) {
     const [visible, setVisible] = useState(false)
 
-    async function initDirectChannel(channelObject) {
-        const users = await loadUsers(channelObject);
-        console.log(`users: ${users}`)
-        let user_ids = Object.keys(users)
-        let receiver = user_ids[0] === props.user.id ? users[1] : users[0];
-        console.log(receiver)
-        return <UserCard name={receiver.username} avatar={receiver.avatar}/>
-    }
 
-    function initGroupChannel(channelObject) {
-        const users = loadUsers(channelObject);
-        return <UserCard name={channelObject.name} avatar={null}/>
-    }
+    // const showChannels = () => {
+    //     let elements = []
+    //     props.channels.forEach(val => {
+    //         console.log(val)
+    //         switch (val['type']) {
+    //             case 'directchannel':
+    //                 initDirectChannel(val).then(e => elements.push(e))
+    //                 break;
+    //             case 'groupchannel':
+    //                 initGroupChannel(val).then(e => elements.push(e))
+    //                 break;
+    //             default:
+    //
+    //
+    //         }
+    //
+    //         elements.push(<UserButton
+    //             avatar={val['avatar']}
+    //             username={val['username']}
+    //             setActiveConversation={props.setActiveConversation}/>)
+    //     })
+    //     return elements
+    // }
 
-    const showChannels = () => {
-        let elements = []
-        props.channels.forEach(val => {
-            console.log(val)
-            switch (val['type']) {
-                case 'directchannel':
-                    initDirectChannel(val).then(e => elements.push(e))
-                    break;
-                case 'groupchannel':
-                    initGroupChannel(val).then(e => elements.push(e))
-                    break;
-                default:
-
-
-            }
-
-            elements.push(<UserButton
-                avatar={val['avatar']}
-                username={val['username']}
-                setActiveConversation={props.setActiveConversation}/>)
-        })
-        return elements
+    const getChannelComponent = () => {
+        return <UserButton avatar={this.avatar} username={this.title}
+                           setActiveConversation={props.setActiveConversation}/>
     }
 
     return (<div className='h-screen w-16 fixed bg-orange-400 shadow-2xl hover:w-96 sidebar'
@@ -96,14 +53,14 @@ export default function Sidebar(props) {
             <SearchField visible={visible}/>
 
             <div className='w-full overflow-x-hidden overflow-y-scroll border-y-2 border-gray-700 buttons'>
-                {/*<UserButton avatar={avatar} username='Dane' visible={visible} setActiveConversation={props.setActiveConversation}/>*/}
-                {/*<UserButton avatar={avatar} username='Chci' visible={visible} setActiveConversation={props.setActiveConversation}/>*/}
-                {/*<UserButton avatar={avatar} username='Ti' visible={visible} setActiveConversation={props.setActiveConversation}/>*/}
-                {/*<UserButton avatar={avatar} username='Cucat' visible={visible} setActiveConversation={props.setActiveConversation}/>*/}
-                {/*<UserButton avatar={avatar} username='Ty Tvoje Plesnivý' visible={visible} setActiveConversation={props.setActiveConversation}/>*/}
-                {/*<UserButton avatar={avatar} username='Palce' visible={visible} setActiveConversation={props.setActiveConversation}/>*/}
-                {/*<UserButton avatar={avatar} username='U Nohou' visible={visible} setActiveConversation={props.setActiveConversation}/>*/}
-                {/*<UserButton avatar={avatar} username='Plsky' visible={visible} setActiveConversation={props.setActiveConversation}/>*/}
+                {/*<UserButton avatar={avatar} username='Dane' visible={visible} setActiveConversation={props.setActiveConversation}/>
+                <UserButton avatar={avatar} username='Chci' visible={visible} setActiveConversation={props.setActiveConversation}/>
+                <UserButton avatar={avatar} username='Ti' visible={visible} setActiveConversation={props.setActiveConversation}/>
+                <UserButton avatar={avatar} username='Cucat' visible={visible} setActiveConversation={props.setActiveConversation}/>
+                <UserButton avatar={avatar} username='Ty Tvoje Plesnivý' visible={visible} setActiveConversation={props.setActiveConversation}/>
+                <UserButton avatar={avatar} username='Palce' visible={visible} setActiveConversation={props.setActiveConversation}/>
+                <UserButton avatar={avatar} username='U Nohou' visible={visible} setActiveConversation={props.setActiveConversation}/>
+                <UserButton avatar={avatar} username='Plsky' visible={visible} setActiveConversation={props.setActiveConversation}/>*/}
                 {/*Object.values(props.channels).forEach(element => {
                     const otherUser = element.users[0] === props.user.id ? element.users[1] : element.users[0];
                     const miniProfile = getMiniProfile(otherUser);
@@ -114,8 +71,14 @@ export default function Sidebar(props) {
                         setActiveConversation: props.setActiveConversation
                     });
                 })*/}
+                {
+                    Array.from(props.storage.channels.values(),
+                        (val, i) =>
+                            <UserButton key={val.id} avatar={val.avatar} username={val.title}
+                                        setActiveConversation={props.setActiveConversation} visible={visible}/>
+                    )
 
-                {showChannels()}
+                }
             </div>
 
             <Signout visible={visible} setUser={props.setUser} clearDesk={props.clearDesk}/>
