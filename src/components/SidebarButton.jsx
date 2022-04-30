b9a2ad0a01c28d6c5fcb41e7994b8eb6a058632a
matousss <matousss33@gmail.com>
2022-04-30 23:48:13 +0200

    Centering "sign out" btn

--- src/components/SidebarButton.jsx
+++ src/components/SidebarButton.jsx
@@ -1,6 +1,6 @@
 export default function (props) {
     return (
-        <button id='expand-btn inline' onClick={() => props.onClick()}
+        <button onClick={() => props.onClick()}
                 className={'h-16 w-full ' + props.className}>
             {props.children}
         </button>
--- src/components/SignOutButton.jsx
+++ src/components/SignOutButton.jsx
@@ -1,5 +1,6 @@
 import React, {useEffect} from 'react';
 import SidebarButton from "./SidebarButton";
+import Avatar from "react-avatar";
 
 function SignOutButton(props) {
     function signOut() {
@@ -12,27 +13,22 @@ function SignOutButton(props) {
 
     return (
         <SidebarButton onClick={() => signOut()}
-                       className={'max-h-[16rem] justify-self-end'}>
-
-            <div className={"h-full w-sidebar float-left"}>
+             className={'justify-self-end flex items-left row align-left w-full'}>
+            <div className={"w-sidebar my-auto"}>
                 <svg xmlns="http://www.w3.org/2000/svg"
-                     className={"h-10 w-full mt-[1rem] ml-[.2rem]"}
+                     className={"h-10 mx-auto ml-[1.1rem]"}
                      fill="none"
                      viewBox="0 0 24 24"
                      stroke="currentColor" strokeWidth={2}>
                     <path strokeLinecap="round" strokeLinejoin="round"
                           d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"/>
-
                 </svg>
-
             </div>
-
-            <div className={"fixed text-xl p-[1.4rem] text-left h-[64px] left-10 on-expand"}>
-                <nobr>
-                    Sign Out
-                </nobr>
+            <div
+                className={
+                    'on-expand text-xl my-auto 0 inline'}>
+                Sign Out
             </div>
-
 
         </SidebarButton>
 
