77592d7c26e958672a76bb8692ae8913c980d9b8
matousss <matousss33@gmail.com>
2022-05-14 00:38:58 +0200

    Little color fix

--- src/components/settings/AvatarSetting.jsx
+++ src/components/settings/AvatarSetting.jsx
@@ -66,7 +66,7 @@ function AvatarSetting() {
                         onChange={onSelectFile}
                     />
                     <div className={'flex'}>
-                        <div className={'h-full pl-1.5 py-2 pr-0 border-r-2 bg-middle/70 rounded-l-2xl'}>
+                        <div className={'h-full pl-1.5 py-2 pr-0 border-r-2 border-ptext/20 bg-middle/70 rounded-l-2xl'}>
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 mr-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12" />
                             </svg>
