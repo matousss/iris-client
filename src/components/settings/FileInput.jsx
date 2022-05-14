53ad240e94d4c3c360a12b75656c2cef357e5794
matousss <matousss33@gmail.com>
2022-05-14 19:57:14 +0200

    small improvement in settings

--- src/components/settings/FileInput.jsx
+++ src/components/settings/FileInput.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-export const FileInput = ({onChange, selected, fileName}) => <label className='block mb-2 w-1/2 text-sm font-medium
-                    rounded-2xl border-2 border-ptext/10 bg-ptext/10 cursor-pointer'>
+export const FileInput = ({onChange, selected, fileName}) => <label className='block mb-2 w-3/5 text-sm font-medium
+                    rounded-3xl border-2 border-ptext/10 bg-ptext/10 cursor-pointer'>
                     <input
                         className={'opacity-0 border-3 border-black w-0 h-0 fixed'}
                         type='file'
@@ -10,9 +10,9 @@ export const FileInput = ({onChange, selected, fileName}) => <label className='b
                     />
                     <div className={'flex'}>
                         <div
-                            className={'h-full pl-1.5 py-2 pr-0 border-r-2 border-ptext/20 bg-middle/70 rounded-l-2xl'}>
+                            className={'h-full pl-1.5 py-1.5 pr-0 border-r-2 border-ptext/20 bg-middle/70 rounded-l-3xl'}>
                             <svg xmlns="http://www.w3.org/2000/svg"
-                                 className="h-6 w-6 mr-1"
+                                 className="h-5 w-5 mr-1"
                                  fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                                 <path strokeLinecap="round" strokeLinejoin="round"
                                       d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"/>
