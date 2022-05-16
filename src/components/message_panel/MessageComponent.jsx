15682637229a28adfb65a1dbd077d691c101c266
matousss <matousss33@gmail.com>
2022-05-17 00:20:22 +0200

    Avatars inside groupchat

--- src/components/message_panel/MessageComponent.jsx
+++ src/components/message_panel/MessageComponent.jsx
@@ -1,21 +1,42 @@
 import React from 'react';
+import {getAvatar} from "../sidebar/ChannelCard";
 
-export function MessageComponent({from, author, children}) {
+export function MessageComponentBase({from, author, children}) {
     return (
         <>
 
-            <div className={'max-w-3/5 lg:max-w-1/2 max-h-[30rem] mx-5 mb-2 ' + (from ? 'incoming-message' : 'ml-auto')}>
-                <label className={'text-sm text-ptext/60 ml-3 hidden incoming-only'}>
-                    {author.username}
-                </label>
-                <div
-                    className={'message-body'}>
-                    {children}
-                </div>
+            <div
+                className={'max-w-3/5 lg:max-w-1/2 max-h-[30rem] mx-5 mb-2 ' + (from ? 'incoming-message' : 'ml-auto')}>
+                {children}
             </div>
         </>
     );
 }
+
+const AuthoredIncomingMessage = ({from, author, children}) => <MessageComponentBase from={from}>
+
+    <label className={'text-sm text-ptext/60 ml-10'}>
+        {author.username}
+
+    </label>
+    <div className={'flex'}>
+        <div className={'mr-2 mt-auto max-h-[32px]'}>
+            {getAvatar({...author, size: 32})}
+        </div>
+        <div className={'message-body authored-message'}>
+            {children}
+        </div>
+    </div>
+</MessageComponentBase>
+
+const PlainMessage = props => <MessageComponentBase {...props}>
+    <div className={'message-body'}>
+        {props.children}
+    </div>
+</MessageComponentBase>
+
+export const MessageComponent = (props) => (props.from && props.author ?
+    <AuthoredIncomingMessage {...props}/> : <PlainMessage {...props}/>)
 
 export const ImageMessageComponent = (props) => <MessageComponent {...props}>
     {props.children}
